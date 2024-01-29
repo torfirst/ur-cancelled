eb5ec186d86b34bd383f8efb6af4b24fa1e4d2fb
tor first <victoriah.first@gmail.com>
2024-01-28 19:56:26 -0600

    deploying code to heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = process.env.PORT || 3306;
+// const PORT = process.env.PORT || 3306;
 
 const hbs = exphbs.create({});
 
